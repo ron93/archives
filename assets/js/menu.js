db16e3e75e9e415d7da6fc8e0eeabb851ea9e576
ron93 <ronmkare1@gmail.com>
2017-03-05 19:36:46 +0300

    javascript catching elements

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,4 +1,16 @@
 var Menu = (function() {
-	
+	// main container
+var $container = $( '#mn-container' ),						
+	// the cover, middle and right panels
+	$cover = $container.find( 'div.mn-cover' ),
+	$middle = $container.find( 'div.mn-middle' ),
+	$right = $container.find( 'div.mn-right' ),
+	// open and close elements
+	$open = $cover.find('a.mn-button-open'),
+	$close = $right.find('span.mn-close'),
+	// the links for each recipe (photo and details)
+	$details = $container.find( 'a.mn-viewdetails' ),
+    
+    
 
 })();
