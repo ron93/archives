7699d83417d9915933f926c8386a27599cc5e9f1
ron93 <ronmkare1@gmail.com>
2021-01-23 02:39:30 +0300

    ChatPage: searchbar added

--- lib/screens/chatPage.dart
+++ lib/screens/chatPage.dart
@@ -42,7 +42,27 @@ class _ChatPageState extends State<ChatPage>{
                   ],
                 ),
               ),
+              
             ),
+            SafeArea(child: Padding(
+              padding: EdgeInsets.only(top: 16,left: 16,right: 16),
+              child: TextField(
+                decoration: InputDecoration(
+                  hintText: "Search...",
+                  hintStyle: TextStyle(color: Colors.grey.shade600),
+                  prefixIcon: Icon(Icons.search,color: Colors.grey.shade600, size: 20,),
+                  filled: true,
+                  fillColor: Colors.grey.shade100,
+                  contentPadding: EdgeInsets.all(8),
+                  enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20),
+                      borderSide: BorderSide(
+                          color: Colors.grey.shade100
+                      )
+                  ),
+                ),
+              ),
+            ),),
           ],
         ),
       ),
