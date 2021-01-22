e4d34513109dcf50923ee6d79bbc5a5dbc18a85f
ron93 <ronmkare1@gmail.com>
2021-01-23 01:59:35 +0300

    homePage: BottomNavigation

--- lib/screens/homePage.dart
+++ lib/screens/homePage.dart
@@ -7,6 +7,28 @@ class HomePage extends StatelessWidget{
       body: Container(
         child: Center(child: Text("Chat")),
       ),
+
+      bottomNavigationBar: BottomNavigationBar(
+        selectedItemColor: Colors.red,
+        unselectedItemColor: Colors.grey.shade600,
+        selectedLabelStyle: TextStyle(fontWeight: FontWeight.w600),
+        unselectedLabelStyle: TextStyle(fontWeight: FontWeight.w600),
+        type: BottomNavigationBarType.fixed,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.message),
+            title: Text("Chats"),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.group_work),
+            title: Text("Channels"),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.account_box),
+            title: Text("Profile"),
+          ),
+        ],
+      ),
     );
   }
 }
