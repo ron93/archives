e533665843fd8b19b04ea05db8433922e6c71b27
ron93 <ronmkare1@gmail.com>
2021-01-23 03:36:54 +0300

    ChatDetailPage : message list

--- lib/screens/chatDetailPage.dart
+++ lib/screens/chatDetailPage.dart
@@ -9,7 +9,13 @@ class ChatDetailPage extends StatefulWidget {
 
 class _ChatDetailPageState extends State<ChatDetailPage>{
 
-
+  List<ChatMessage> messages = [
+    ChatMessage(messageContent: "Hello, Will", messageType: "receiver"),
+    ChatMessage(messageContent: "How have you been?", messageType: "receiver"),
+    ChatMessage(messageContent: "Hey Kriss, I am doing fine dude. wbu?", messageType: "sender"),
+    ChatMessage(messageContent: "ehhhh, doing OK.", messageType: "receiver"),
+    ChatMessage(messageContent: "Is there any thing wrong?", messageType: "sender"),
+  ];
 
   @override
   Widget build(BuildContext context) {
